42bab23c26c43d908aa22a8c881d3aff4a807599
ismilawd <ismilawd@gmail.com>
2022-08-14 18:31:04 +0430

    Bind Actions

--- Code/Components/Player.cpp
+++ Code/Components/Player.cpp
@@ -19,3 +19,25 @@ namespace
 
 	CRY_STATIC_AUTO_REGISTER_FUNCTION(&RegisterPlayerComponent);
 }
+
+void CPlayerComponent::Initialize()
+{
+	m_pCameraComponent = m_pEntity->GetOrCreateComponent<Cry::DefaultComponents::CCameraComponent>();
+	m_pInputComponent = m_pEntity->GetOrCreateComponent<Cry::DefaultComponents::CInputComponent>();
+	m_pCharacterController = m_pEntity->GetOrCreateComponent<Cry::DefaultComponents::CCharacterControllerComponent>();
+	m_pAdvancedAnimationComponent = m_pEntity->GetOrCreateComponent<Cry::DefaultComponents::CAdvancedAnimationComponent>();
+
+	m_pInputComponent->RegisterAction("player", "moveforward", [this](int activationMode, float value) {m_movementDelta.y = value; });
+	m_pInputComponent->BindAction("player", "moveforward", eAID_KeyboardMouse, eKI_W);
+
+	m_pInputComponent->RegisterAction("player", "movebackward", [this](int activationMode, float value) {m_movementDelta.y = -value; });
+	m_pInputComponent->BindAction("player", "movebackward", eAID_KeyboardMouse, eKI_S);
+
+	m_pInputComponent->RegisterAction("player", "moveright", [this](int activationMode, float value) {m_movementDelta.x = value; });
+	m_pInputComponent->BindAction("player", "moveright", eAID_KeyboardMouse, eKI_D);
+
+	m_pInputComponent->RegisterAction("player", "moveleft", [this](int activationMode, float value) {m_movementDelta.x = -value; });
+	m_pInputComponent->BindAction("player", "moveleft", eAID_KeyboardMouse, eKI_A);
+
+	m_pEntity->SetPos(m_pEntity->GetWorldPos() + Vec3(m_movementDelta.x, m_movementDelta.y, 0));
+}
--- Code/Components/Player.h
+++ Code/Components/Player.h
@@ -1,6 +1,11 @@
 // Copyright 2017-2020 Crytek GmbH / Crytek Group. All rights reserved.
 #pragma once
 
+#include <DefaultComponents/Cameras/CameraComponent.h>
+#include <DefaultComponents/Input/InputComponent.h>
+#include <DefaultComponents/Physics/CharacterControllerComponent.h>
+#include <DefaultComponents/Geometry/AdvancedAnimationComponent.h>
+
 ////////////////////////////////////////////////////////
 // Represents a player participating in gameplay
 ////////////////////////////////////////////////////////
@@ -10,9 +15,23 @@ public:
 	CPlayerComponent() = default;
 	virtual ~CPlayerComponent() = default;
 
+	virtual void Initialize() override;
+
 	// Reflect type to set a unique identifier for this component
 	static void ReflectType(Schematyc::CTypeDesc<CPlayerComponent>& desc)
 	{
 		desc.SetGUID("{63F4C0C6-32AF-4ACB-8FB0-57D45DD14725}"_cry_guid);
 	}
+private:
+	//camera
+	//input
+	//Character Controller
+	//Advanced Animation
+
+	Cry::DefaultComponents::CCameraComponent* m_pCameraComponent;
+	Cry::DefaultComponents::CInputComponent* m_pInputComponent;
+	Cry::DefaultComponents::CCharacterControllerComponent* m_pCharacterController;
+	Cry::DefaultComponents::CAdvancedAnimationComponent* m_pAdvancedAnimationComponent;
+
+	Vec2 m_movementDelta;
 };
